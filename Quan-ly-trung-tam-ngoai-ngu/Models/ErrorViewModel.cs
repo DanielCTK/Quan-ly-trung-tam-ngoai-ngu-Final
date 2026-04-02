32fb968dc102f64b7b7f73265056866ece1ba737
NDuyPhuc <109029567+NDuyPhuc@users.noreply.github.com>
2026-04-02 08:04:02 +0700

    Add project files.

---
+++ Quan-ly-trung-tam-ngoai-ngu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quan_ly_trung_tam_ngoai_ngu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
